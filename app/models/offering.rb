5c75dd6684f6c7ecd32087b2ba42b7bf52312434
Brian Smith <smith.brianr@gmail.com>
2013-12-07 10:00:07 -0600

    Revert "Additional Changes w/ login credentials required."
    This reverts commit c2ca68894c3c3431a5e0aedf817d44413ea84ffc.

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -1,7 +1,6 @@
 class Offering < ActiveRecord::Base
   belongs_to :company
   belongs_to :procedure
-  belongs_to :category
   has_many :locations, :through => :company
 
   # belongs_to :company is simply shorthand for:
